e02eac83ad905c65d2c2e3b9d0bfbc547a5f7afe
zaynab10o <zaynabm766@gmail.com>
2025-04-13 17:26:49 +0200

    Add logout link to sidebar for improved navigation

--- resources/views/layout/tmp.blade.php
+++ resources/views/layout/tmp.blade.php
@@ -16,6 +16,8 @@
         <a href="{{ route('in') }}">الرئيسية</a>
         <a href="{{ route('index') }}">اصحاب الحرف</a>
         <a href="{{ route('index_category') }}">جميع الحرف</a>
+        <a href="{{ route('index_category') }}">تسجيل الخروج</a>
+
     
         <form class="group-btn" action="{{ route('logout') }}" method="POST">
             @csrf
