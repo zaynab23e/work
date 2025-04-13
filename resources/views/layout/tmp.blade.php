b927db57a916baa8648943b1b83fa299aa91fbe9
zaynab10o <zaynabm766@gmail.com>
2025-04-13 17:22:33 +0200

    Refactor sidebar styles for improved layout and transition effects

--- resources/views/layout/tmp.blade.php
+++ resources/views/layout/tmp.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-   @yield('title')
-   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    @yield('title')
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body> 
     <!-- Sidebar -->
@@ -22,7 +22,6 @@
             <button type="submit" class="logout-button">تسجيل الخروج</button>
         </form>
     </div>
-       
 
     @yield('conntent')
 
