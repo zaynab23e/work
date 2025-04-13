cf6655a6fe601f5a428b70d8713334f18949a9a7
zaynab10o <zaynabm766@gmail.com>
2025-04-13 16:57:49 +0200

    Refactor logout button markup for improved structure and styling

--- resources/views/layout/tmp.blade.php
+++ resources/views/layout/tmp.blade.php
@@ -19,8 +19,22 @@
     
         <form class="group-btn" action="{{ route('logout') }}" method="POST">
             @csrf
-            <button type="submit" class="logout-button">تسجيل الخروج</button>
-        </form>
+            <button type="submit" 
+            class="logout-button" 
+            style="text-decoration: none;
+                   color: white;
+                   padding: 12px 18px;
+                   margin: 10px 0;
+                   background-color: #525293;
+                   border-radius: 5px;
+                   text-align: center;
+                   width: 100%;
+                   border: none;
+                   cursor: pointer;
+                   display: none;">
+            تسجيل الخروج
+        </button>
+                </form>
     </div>
     
 
