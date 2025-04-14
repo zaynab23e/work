599348b38b289842bc44c58315c1840928bc3bb8
zaynab10o <zaynabm766@gmail.com>
2025-04-14 10:37:02 +0200

    Fix typo in section directive from 'content' to 'contennt'

--- resources/views/work/show.blade.php
+++ resources/views/work/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.tmp')
 
-@section('content')
+@section('contennt')
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
