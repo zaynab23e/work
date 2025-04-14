f004a8a58ec287be2a4d279e6fc73a7810e69e68
zaynab10o <zaynabm766@gmail.com>
2025-04-14 10:33:15 +0200

    Refactor layout structure for improved readability and organization

--- resources/views/work/show.blade.php
+++ resources/views/work/show.blade.php
@@ -1,7 +1,5 @@
 @extends('layout.tmp')
 
-@section('title', 'Employee Profile')
-
 @section('content')
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
